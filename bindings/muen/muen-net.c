4560f69ca2f9b75752b41c37d5b6266deecc73d7
Sven Anderson <sven@anderson.de>
2024-08-22 06:13:33 +0200

    deterministic mac addresses

--- bindings/muen/muen-net.c
+++ bindings/muen/muen-net.c
@@ -128,25 +128,16 @@ bool muen_net_pending_data(solo5_handle_t handle)
 
 static void generate_mac_addr(uint8_t *addr)
 {
-    const uint64_t dsize = sizeof(uint64_t) * 8;
-    const char *subject_name = muen_get_subject_name();
-    static uint64_t counter = 0;
-    uint64_t data;
-    int i;
-
-    data  = (muen_get_sched_start() << 32) | muen_get_sched_end();
-    data ^= tscclock_epochoffset();
-    data  = ((data << counter) | (data >> (dsize - counter))) + counter;
-    counter = (counter + 1) % dsize;
-
-    for (i = 0; i < 6; i++)
+    static uint8_t counter = 1;
+    static const uint64_t data = 0x0a0b0c0d0e00;
+    for (int i = 0; i < 6; i++)
     {
-        addr[i]  = subject_name[i];
-        addr[i] ^= (uint8_t)(data >> (i * 8));
+        addr[i] = (uint8_t)(data >> ((5-i) * 8));
     }
+    addr[5] += counter++;
 
     /* clear multicast and set local assignment bit */
-    addr[0] &= 0xfe;
+    addr[0] &= ~0x01;
     addr[0] |= 0x02;
 }
 
